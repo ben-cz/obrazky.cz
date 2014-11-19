b9ed601a602cc4069d494b381ad764c4a076e37b
Jakub Dohnal <dohnal.kuba@gmail.com>
2014-11-19 19:47:48 +0100

    Initial Commit

---
+++ obrazky/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  obrazky
+//
+//  Created by Jakub Dohnal on 19/11/14.
+//  Copyright (c) 2014 dohnal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
